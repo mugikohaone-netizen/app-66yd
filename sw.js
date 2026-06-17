fba1f114257f25847e5cd462fc72bdf573dae736
mugikohaone-netizen <mugikoha.one@gmail.com>
2026-06-18 04:25:14 +0900

    🔄 SW更新 vmqigni0x

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-mqigmhvl';
+const CACHE = 'app-mqigni0x';
 const FILES = ['./','./index.html'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting()));
